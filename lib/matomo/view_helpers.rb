57b6034bd46c5cd84d28155947aed2a8dec87af3
Vivian Brown <vivian@eff.org>
2019-03-08 11:58:03 -0500

    Optionally set page_title as param

--- lib/matomo/view_helpers.rb
+++ lib/matomo/view_helpers.rb
@@ -1,23 +1,31 @@
 module Matomo
   module ViewHelpers
-    def matomo_tracking_url
+    def matomo_tracking_embed(opts = {})
+      content_tag(:div, id: "anon-stats") do
+        content_tag(:noscript) do
+          tag(:img, src: matomo_tracking_url(opts), style: "border:0", alt: "")
+        end +
+        javascript_tag do
+          "document.getElementById('anon-stats').innerHTML = '<img src=\"#{matomo_tracking_url(opts)}\"?urlref=' + encodeURIComponent(document.referrer) + 'style=\"border:0\" alt=\"\" />';".html_safe
+        end
+      end
+    end
+
+    def matomo_tracking_url(opts = {})
       "#{Matomo.base_url}/js/?" + {
         idsite: Matomo.site_id,
         rec: 1,
-        action_name: page_title,
+        action_name: action_name_or_default(opts[:action_name]),
         url: request.original_url
-      }.to_param
+      }.compact.to_param
     end
 
-    def matomo_tracking_embed
-      content_tag(:div, id: "anon-stats") do
-        content_tag(:noscript) do
-          tag(:img, src: matomo_tracking_url, style: "border:0", alt: "")
-        end +
-        javascript_tag do
-          "document.getElementById('anon-stats').innerHTML = '<img src=\"#{matomo_tracking_url}\"?urlref=' + encodeURIComponent(document.referrer) + 'style=\"border:0\" alt=\"\" />';".html_safe
-        end
-      end
+    private
+
+    def action_name_or_default(name)
+      return name unless name.nil?
+      return page_title if defined?(page_title)
+      return nil
     end
   end
 end
